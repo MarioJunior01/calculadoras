13635294c73ee3fb6b9d5bb5192cbe2f32491a0b
Mario Junior <136728961+MarioJunior01@users.noreply.github.com>
2025-10-02 03:07:50 +0000

    atividade finalizada

--- src/componets/telas/Tela2.js
+++ src/componets/telas/Tela2.js
@@ -1,9 +1,97 @@
 import React from "react";
 
+const estilos = {
+  btn: {
+    backgroundColor: "red",
+    color: "white",
+    padding: "5px 50px",
+    margin: "20px 10px",
+    borderRadius: 20,
+  },
+  inputs: {
+    padding: "2px",
+    margin: "7px",
+  },
+  equacao: {
+    margin: "7px",
+  },
+  resultado: {
+    whiteSpace: "pre-line",
+  },
+};
 export default function Tela2() {
+  const [valorA, setValorA] = React.useState();
+  const [valorB, setValorB] = React.useState();
+  const [valorC, setValorC] = React.useState();
+  const [delta, setDelta] = React.useState();
+  const [x1, setX1] = React.useState();
+  const [x2, setX2] = React.useState();
+
   return (
     <div>
-      <h3> Tela 2</h3>
+      <h3> FÒRMULA DE BHASKARA </h3>
+      <div>
+        <label style={estilos.labels}> Digite o valor de A:</label>
+        <input
+          type="number"
+          name="valorA"
+          style={estilos.inputs}
+          value={valorA}
+          onChange={(e) => setValorA(e.target.value)}
+        />
+        <label> Digite o valor de B:</label>
+        <input
+          type="number"
+          name="valorB"
+          style={estilos.inputs}
+          value={valorB}
+          onChange={(e) => setValorB(e.target.value)}
+        ></input>
+        <label> Digite o valor de C:</label>
+        <input
+          type="number"
+          name="valorC"
+          style={estilos.inputs}
+          value={valorC}
+          onChange={(e) => setValorC(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <p style={estilos.equacao}>
+          Equação formada: {valorA}x²
+          {valorB < 0 ? `${valorB}x` : `+${valorB}x`}
+          {valorC < 0 ? `${valorC}` : `+${valorC}`}
+        </p>
+        <button style={estilos.btn} onClick={() => calcular()}>
+          {" "}
+          Calcular{" "}
+        </button>
+        <p> Valor de Delta:{delta}</p>
+        <br></br>
+        <p>RESULTADO</p>
+        <br></br>
+        <p style={estilos.resultado}>
+          {delta <= 0
+            ? `A equacão não possuì raizes, pois o delta é negativo  `
+            : ` Raiz 1 =  ${x1}` + "\n" + `Raiz 2 =  ${x2}`}
+        </p>
+      </div>
     </div>
   );
+
+  function calcular() {
+    let a = parseFloat(valorA);
+    let b = parseFloat(valorB);
+    let c = parseFloat(valorC);
+
+    let valorDelta = Math.pow(b, 2) - 4 * a * c;
+    setDelta(valorDelta);
+
+    if (valorDelta >= 0) {
+      let valorX1 = (-b + Math.sqrt(valorDelta)) / (2 * a);
+      let valorX2 = (-b - Math.sqrt(valorDelta)) / (2 * a);
+      setX1(valorX1.toFixed(2));
+      setX2(valorX2.toFixed(2));
+    }
+  }
 }
--- src/componets/telas/Tela3.js
+++ src/componets/telas/Tela3.js
@@ -1,9 +1,52 @@
 import React from "react";
 
+const estilos = {
+  btn: {
+    backgroundColor: "purple",
+    color: "white",
+    padding: "5px 15px",
+    margin: "20px 10 px",
+    borderRadius: 10,
+  },
+  btn1: {
+    backgroundColor: "green",
+    color: "white",
+    padding: "5px 15px",
+    margin: "20px 5px",
+    borderRadius: 10,
+  },
+};
+
 export default function Tela3() {
+  //variaveis de estado
+  const [num1, setNum1] = React.useState(10);
+  const [res, setRes] = React.useState();
+
+  function Converter() {
+    //CASTIN String to number
+    let n1 = Number(num1);
+    let pa1 = n1 * 1.8 + 32;
+
+    setRes(pa1);
+  }
+
   return (
     <div>
-      <h3>Tela 3 </h3>
+      <h3>Calculadora de Graus e Fahrenheit</h3>
+      <div>
+        <label htmlFor="num1">Graus </label>
+        <input
+          type="number"
+          name="num1"
+          value={num1}
+          onChange={(e) => setNum1(e.target.value)}
+        />{" "}
+        <br /> <br />
+        <button style={estilos.btn} onClick={() => Converter()}>
+          Converter
+        </button>
+        <p>resultado em Fahrenheit : {res}</p>
+      </div>
     </div>
   );
 }
